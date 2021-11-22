89887b7e2ce00cd924a987eb4ecb50fe41292da4
jessica <jessicathaynnara@gmail.com>
2021-11-22 16:07:12 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-window.onload = function() {
-  let black = document.getElementsByClassName('color')
-  black[0].className= 'color selected'
-}
-function seleciona (){
-  let paleta = document.getElementsByClassName('color')
-  for (let i=0; i<paleta.length; i+=1){ 
-    paleta[i].addEventListener('click',function(event){
-        for(let index=0; index<paleta.length; index+=1){
-            paleta[index].className = 'color'
-        }
-        event.target.classList = 'color selected'
-    })
+window.onload = function () {
+  const black = document.getElementsByClassName('color');
+  black[0].className = 'color selected';
+};
+function seleciona() {
+  const paleta = document.getElementsByClassName('color');
+  for (let i = 0; i < paleta.length; i += 1) {
+    paleta[i].addEventListener('click', function (event) {
+      for (let index = 0; index < paleta.length; index += 1) {
+        paleta[index].className = 'color';
+      }
+      event.target.classList = 'color selected';
+    });
   }
-
 }
 seleciona();
+function colore() {
+  const blocos = document.getElementsByClassName('pixel');
+  for (let i = 0; i < blocos.length; i += 1) {
+    blocos[i].addEventListener('click', function (event) {
+      const corselecionada = document.querySelector('.selected');
+      event.target.style.backgroundColor = corselecionada.style.backgroundColor;
+    });
+  }
+}
+colore();
